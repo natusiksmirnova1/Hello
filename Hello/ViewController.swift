e987a1ea5a5b8ec829ab30b25a58020dc84ad77f
Natasha <natusiksmirnova1@gmail.com>
2023-06-08 15:18:58 +0300

    Code for button action

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var label: UILabel!
+    
+    @IBOutlet var toggleButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.isHidden = true
+        toggleButton.layer.cornerRadius = 10
+        
     }
 
-
+    @IBAction func makeAction() {
+        label.isHidden.toggle()
+        label.isHidden ?  toggleButton.setTitle("Text is hidden", for: .normal) :  toggleButton.setTitle("Text is shown", for: .normal)
+    }
+    
 }
 
